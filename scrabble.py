c4864b0005a69a67b2406f8ff261b8cbe1abb110
Rob Spotts <rob.spotts@gmail.com>
2015-08-12 15:10:13 -0400

    Update scrabble.py

--- scrabble.py
+++ scrabble.py
@@ -7,7 +7,7 @@
 
 import sys
 
-# Dictionary containing all letters and their Scrabble values
+# Dictionary containing all letters and their Scrabble point values
 scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
           "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
           "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
